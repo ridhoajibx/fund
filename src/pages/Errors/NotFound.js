38b21d33e8699ac9390e206cc963f4a8f6cd28f8
ridhoajibx <ridhoajibx@gmail.com>
2020-12-18 06:53:03 +0700

    [ridho] - create settings page

--- src/pages/Errors/NotFound.js
+++ src/pages/Errors/NotFound.js
@@ -1,16 +1,35 @@
 import { Link } from 'react-router-dom';
 import Container from '../../components/Container';
+import authBg from '../../assets/img/register_bg_2.png';
+import { FaHome } from 'react-icons/fa';
 
 const NotFound = () => {
     return (
-        <Container>
-            <div className="flex flex-col h-screen justify-center items-center mx-auto">
-                <h3>404 This page not found</h3>
-                <div className="mt-2">
-                    <Link className="px-4 py-2 bg-purple-600 shadow rounded-lg focus:ring-2 focus:ring-purple-200 text-white" to="/">Go Back to Home</Link>
+        <section className="relative w-full h-full py-40 min-h-screen">
+            <div
+                className="absolute top-0 w-full h-full bg-purple-600 bg-no-repeat bg-full"
+                style={{
+                    backgroundImage:
+                        "url(" + authBg + ")",
+                }}
+            ></div>
+            <div className="container m-auto px-4 h-full">
+                <div className="flex content-center items-center justify-center h-full">
+                    <div className="w-full lg:w-6/12 px-4">
+                        <div className="relative flex flex-col items-center justify-center min-w-0 p-10 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
+                            <h3 className="text-2xl mb-2 uppercase">404 This page not found</h3>
+                            <div className="mt-2 uppercase">
+                                <Link className="px-4 py-2 bg-purple-600 shadow rounded focus:ring-2 focus:ring-purple-200 text-white font-bold text-sm flex items-center" to="/">
+                                    <FaHome className="mr-2" />
+                                    Home
+                                </Link>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
             </div>
-        </Container>
+        </section>
     );
 }
 
--- src/routes/IndexRoutes.js
+++ src/routes/IndexRoutes.js
@@ -1,14 +1,15 @@
 import { useState } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import PrivateRoutes from './PrivateRoutes';
+import MainLayouts from '../layouts/MainLayouts';
 import AppLayouts from '../layouts/AppLayouts';
 import AuthLayouts from '../layouts/AuthLayouts';
-import MainLayouts from '../layouts/MainLayouts';
-import Dashboard from '../pages/Apps/Dashboard';
+import Home from '../pages/Home/Home';
 import Login from '../pages/Auth/Login';
 import Register from '../pages/Auth/Register';
+import Dashboard from '../pages/Apps/Dashboard';
+import Settings from '../pages/Apps/Settings';
 import NotFound from '../pages/Errors/NotFound';
-import Home from '../pages/Home/Home';
-import PrivateRoutes from './PrivateRoutes';
 
 const IndexRoutes = () => {
     const [auth, setAuth] = useState(false);
@@ -43,6 +44,13 @@ const IndexRoutes = () => {
             <PrivateRoutes exact path="/dashboard"
                 comp={AppLayouts}
                 child={Dashboard}
+                setAuth={setAuth}
+                auth={auth}
+            />
+
+            <PrivateRoutes exact path="/settings"
+                comp={AppLayouts}
+                child={Settings}
                 setAuth={setAuth}
                 auth={auth}
             />
