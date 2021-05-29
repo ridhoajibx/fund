8a8f10dde2e17a1be715396bc6526de301ca5fee
ridhoajibx <ridhoajibx@gmail.com>
2021-05-29 10:42:08 +0700

    [ridho] - new

--- src/components/Navbars/AppNavbar.js
+++ src/components/Navbars/AppNavbar.js
@@ -9,7 +9,7 @@ const AppNavbar = (props) => {
         <>
             {/* Navbar */}
             <nav className="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-no-wrap md:justify-start flex items-center p-4">
-                <div className="w-full mx-autp items-center flex justify-between md:flex-no-wrap flex-wrap md:px-10 px-4">
+                <div className="w-full mx-auto items-center flex justify-between md:flex-no-wrap flex-wrap md:px-10 px-4">
                     {/* Brand */}
                     <a
                         className="text-white text-sm uppercase hidden lg:inline-block font-extrabold"
